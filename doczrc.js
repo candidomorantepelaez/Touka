ea9c3dd43a4d245b38270f1404850c580dd35efa
candido morante <candidomorantepelaez@hotmail.com>
2018-12-14 22:30:13 +0100

    configurando docz

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,13 @@
 export default {
-  src: './',
+  src: './docsDev',
   dest: './docs',
   base: '/touka/',
   title: 'Touka',
   description: 'Framework open source foe react app',
+  themeConfig: {
+    logo: {
+      src: './public/assets/images/logoWithName.png',
+      width: 150,
+    },
+  },
 }
