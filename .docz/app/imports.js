6eac813dfc403cd8f32f727ea4a9523655e346f6
candido morante <candidomorantepelaez@hotmail.com>
2018-12-15 13:07:52 +0100

    esta ya es la buena

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,8 @@
 export const imports = {
+  'create-app-touka.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "create-app-touka" */ 'create-app-touka.mdx'),
+  'gestion-react.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "gestion-react" */ 'gestion-react.mdx'),
   'index.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.mdx'),
 }
---
+++ docs/static/js/6.f7464a57.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"./.docz/app/imports.js":function(n,e,t){"use strict";t.r(e),t.d(e,"imports",function(){return o});var o={"create-app-touka.mdx":function(){return t.e(1).then(t.bind(null,"./docsDev/create-app-touka.mdx"))},"gestion-react.mdx":function(){return t.e(2).then(t.bind(null,"./docsDev/gestion-react.mdx"))},"index.mdx":function(){return t.e(3).then(t.bind(null,"./docsDev/index.mdx"))}}}},0,[1,2,3]]);
+//# sourceMappingURL=6.928d79e5918f33549d07.js.map
