48a4ea49a57b0f380db6d717ccac7779d1651136
candido <candidomorantepelaez@hotmail.com>
2019-02-15 18:12:54 +0100

    se realizan mejoras generales

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { getMenu, getRoutes, getPageRewiew } from 'modules'
 import getStore from 'store'
 import { messages, initDefaultLocales } from 'i18n'
 import setGlobalData from 'config/set-global-data'
-import 'typeface-roboto'
 
 
 const createAppTouka = (userConfig = {}) => {
